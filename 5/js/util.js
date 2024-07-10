07315d626ea0ce0b808f3fee401593d7c8ee78ab
Keks <keks@htmlacademy.ru>
2024-07-11 01:53:16 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/util.js
@@ -0,0 +1,43 @@
+function generateRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generateUniqueId() {
+  const usedIdNumbers = [];
+  return function () {
+    let randomId;
+
+    do {
+      randomId = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+    } while (usedIdNumbers.includes(randomId));
+
+    usedIdNumbers.push(randomId);
+    return randomId;
+  };
+}
+
+function getRandomString(array) {
+  const randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+}
+
+function generateUniqueNumbers() {
+  const usedNumbers = [];
+  return function (min, max) {
+    let randomNumber;
+    do {
+      randomNumber = generateRandomNumber(min, max);
+    } while (usedNumbers.includes(randomNumber));
+
+    usedNumbers.push(randomNumber);
+
+    return randomNumber;
+  };
+}
+
+export {
+  generateRandomNumber,
+  generateUniqueId,
+  getRandomString,
+  generateUniqueNumbers
+};
