58c6ee35ed3099587bdafc7e68ee8da288f53b40
Ancogni <niksroman84@gmail.com>
2024-08-01 21:35:20 +0300

    Перламутровые пуговицы

--- js/gallery.js
+++ js/gallery.js
@@ -3,21 +3,23 @@ import {showBigPicture} from './big-picture.js';
 
 const container = document.querySelector('.pictures');
 
-const renderGallery = (pictures) => {
-  container.addEventListener('click', (evt) => {
-    const thumbnail = evt.target.closest('[data-thumbnail-id]');
-    if (!thumbnail) {
-      return;
-    }
+let pictures = [];
 
-    evt.preventDefault();
-    const picture = pictures.find(
-      (item) => item.id === +thumbnail.dataset.thumbnailId
-    );
-    showBigPicture(picture);
-  });
+const onContainerClick = (evt) => {
+  const thumbnail = evt.target.closest('[data-thumbnail-id]');
+  if (!thumbnail) {
+    return;
+  }
 
+  evt.preventDefault();
+  const picture = pictures.find((item) => item.id === +thumbnail.dataset.thummbnailId);
+  showBigPicture(picture);
+};
+
+const renderGallery = (currentPictures) => {
+  pictures = currentPictures;
   generateThumbnails(pictures, container);
+  container.addEventListener('click', onContainerClick);
 };
 
 export {renderGallery};
