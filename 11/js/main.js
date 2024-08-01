5b49fb2a7f706fb0788e4d7c7f3f73082766d0fc
Keks <keks@htmlacademy.ru>
2024-08-01 15:13:10 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/main.js
@@ -0,0 +1,22 @@
+import {renderGallery} from './gallery.js';
+import {getData, sendData} from './api.js';
+import {showAlert} from './util.js';
+import {setOnFormSubmit, hideModal} from './form.js';
+import {showSuccessMessage, showErrorMessage} from './message.js';
+
+setOnFormSubmit(async (data) => {
+  try {
+    await sendData(data);
+    hideModal();
+    showSuccessMessage();
+  } catch {
+    showErrorMessage();
+  }
+});
+
+try {
+  const data = await getData();
+  renderGallery(data);
+} catch {
+  showAlert();
+}
---
+++ 11/js/util.js
@@ -0,0 +1,14 @@
+const ALERT_SHOW_TIME = 5000;
+
+const dataErrorTemplate = document.querySelector('#data-error').content.querySelector('.data-error');
+
+const showAlert = () => {
+  const dataErrorElement = dataErrorTemplate.cloneNode(true);
+  document.body.append(dataErrorElement);
+
+  setTimeout(() => {
+    dataErrorElement.remove();
+  }, ALERT_SHOW_TIME);
+};
+
+export {showAlert};
